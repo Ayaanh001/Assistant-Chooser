04cf21f22b2bdd5eeb03c7bd65ec0d9bacc1ab4e
Ayaan Hussain <kingaah83@gmail.com>
2026-04-28 22:57:14 +0530

    Improved the GroupSurface logic

--- app/src/main/java/com/hussain/assistantchooser/ui/components/GroupSurface.kt
+++ app/src/main/java/com/hussain/assistantchooser/ui/components/GroupSurface.kt
@@ -11,24 +11,28 @@ import androidx.compose.ui.unit.dp
 @Composable
 fun GroupSurface(
     count: Int,
+    modifier: Modifier = Modifier,
     content: @Composable (index: Int, shape: RoundedCornerShape) -> Unit
 ) {
-    Column {
+    Column(modifier = modifier) {
         repeat(count) { i ->
-            val shape = when {
-                count == 1 -> RoundedCornerShape(24.dp)
-                i == 0 -> RoundedCornerShape(
-                    topStart = 24.dp, topEnd = 24.dp,
-                    bottomStart = 8.dp, bottomEnd = 8.dp
-                )
-                i == count - 1 -> RoundedCornerShape(
-                    topStart = 8.dp, topEnd = 8.dp,
-                    bottomStart = 24.dp, bottomEnd = 24.dp
-                )
-                else -> RoundedCornerShape(8.dp)
-            }
-            content(i, shape)
+            content(i, getGroupShape(i, count))
             if (i != count - 1) Spacer(modifier = Modifier.height(3.dp))
         }
+    }
+}
+
+fun getGroupShape(index: Int, count: Int): RoundedCornerShape {
+    return when {
+        count == 1 -> RoundedCornerShape(24.dp)
+        index == 0 -> RoundedCornerShape(
+            topStart = 24.dp, topEnd = 24.dp,
+            bottomStart = 8.dp, bottomEnd = 8.dp
+        )
+        index == count - 1 -> RoundedCornerShape(
+            topStart = 8.dp, topEnd = 8.dp,
+            bottomStart = 24.dp, bottomEnd = 24.dp
+        )
+        else -> RoundedCornerShape(8.dp)
     }
 }
